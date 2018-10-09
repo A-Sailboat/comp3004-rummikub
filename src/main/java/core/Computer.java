30f927dccf0e98604b1465763eccc4589c2908de
Henry <henry.proctor.hp@gmail.com>
2018-10-09 01:30:48 -0400

    -Created Player Superclass 	In Progress. Only certain trivial methods have been implemented so far. -Created Computer (Subclass of Player) 	In Progress. Only certain trivial methods have been implemented so far. -Created Human(Subclass of Player) 	In Progress. Only certain trivial methods have been implemented so far. -Added User Stories 	Added 5 user stories and a template story for the ones to be added

---
+++ src/main/java/core/Computer.java
@@ -0,0 +1,19 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Computer extends Player{
+	
+	public Computer() {}
+
+	
+	@Override
+	public String toString() {
+		return "A Computer player";
+	}
+
+
+}
+
+
---
+++ src/main/java/core/Human.java
@@ -0,0 +1,19 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Human extends Player{
+	
+	public Human() {}
+
+	@Override
+	public String toString() {
+		return "A human player";
+	}
+
+
+}
+
+
